ed428c7b255eefe2bc9ce0882ee6e44300ca1bba
john <john.wee@flex-solver.com.sg>
2020-02-15 21:04:55 +0800

    change services to follow rxjs format and update showcase component

--- src/app/services/showcases/showcases.service.ts
+++ src/app/services/showcases/showcases.service.ts
@@ -1,33 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import {  BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, EMPTY, throwError, Observable } from 'rxjs';
 import { ShowcaseItem } from 'src/app/data/showcase-item';
-
+import { tap, catchError } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ShowcasesService {
 
-  public showCasesSubject:BehaviorSubject<ShowcaseItem[]>=new BehaviorSubject<ShowcaseItem[]>(null);
-  SERVER_URL: string = "http://localhost:8080/api/";
-  constructor(private httpClient:HttpClient) {
-    this.getAllShowCases()
+  SERVER_URL: string = "api/showcases";
+
+
+  constructor(private httpClient: HttpClient) {
   }
 
 
-  private getAllShowCases()
-  {
-
-    let data;
-    this.httpClient.get(this.SERVER_URL + 'showcases').subscribe(response=>
-    {
-      data=response;
-      this.showCasesSubject.next(data);
-      setTimeout(() => {
-        this.getAllShowCases()
-    }, 5000);
-    })
-
+  getAllShowCases(): Observable<ShowcaseItem[]> {
+    return this.httpClient.get<ShowcaseItem[]>(this.SERVER_URL).pipe(
+      tap(data => console.log(data)),
+      catchError(err => {
+        return throwError(err);
+      }))
   }
+
+
+
 }
