82d7ed5a6b3ffab21f94abbb417aa0186b676d60
john <john.wee@flex-solver.com.sg>
2020-03-01 15:43:52 +0800

    add profile image stock

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -4,9 +4,11 @@ import { UserProfileComponent } from "./user-profile/user-profile.component";
 import { RouterModule } from "@angular/router";
 import { routes } from "./profile.routing";
 import { FlexLayoutModule } from "@angular/flex-layout";
+import { HeaderComponent } from './user-profile/header/header.component';
+import { SidenavComponent } from './user-profile/sidenav/sidenav.component';
 
 @NgModule({
-  declarations: [UserProfileComponent],
+  declarations: [UserProfileComponent, HeaderComponent, SidenavComponent],
   // TODO: Choose whether to add SharedModule or FlexLayoutModule
   imports: [CommonModule, FlexLayoutModule, RouterModule.forChild(routes)]
 })
